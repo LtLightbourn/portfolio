5a9c54c78da47fb4d7d5ad12f1cb7f650e91aa95
Lawrence Lightbourn <ltlightbourn@gmail.com>
2026-04-19 22:31:15 -0700

    cover spline watermark

--- components/spline-robot.tsx
+++ components/spline-robot.tsx
@@ -6,6 +6,17 @@ const Spline = dynamic(() => import('@splinetool/react-spline'), { ssr: false })
 
 export default function SplineRobot() {
   return (
-    <Spline scene="https://prod.spline.design/E74YaxYBe2pJTDbH/scene.splinecode" />
+    <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+      <Spline scene="https://prod.spline.design/E74YaxYBe2pJTDbH/scene.splinecode" />
+      <div style={{
+        position: 'absolute',
+        bottom: 0,
+        right: 0,
+        width: '220px',
+        height: '60px',
+        background: '#0a0a0a',
+        zIndex: 10
+      }} />
+    </div>
   );
 }
